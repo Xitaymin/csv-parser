67380007142e3c07a27df12177421deb4278959a
Xitaymin <y.barashenko@gmail.com>
2021-09-18 23:00:09 +0300

    started implementation of csv parser class

--- src/main/java/com/xitaymin/CsvParser.java
+++ src/main/java/com/xitaymin/CsvParser.java
@@ -1,13 +1,38 @@
 package com.xitaymin;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CsvParser {
+    private String filePath;
+
     public CsvParser(String filePath) {
-        // todo implement
+        this.filePath = filePath;
+        //todo check if file exists
     }
-    public <T> List<T> parseLines(Class<T>) {
-        // todo implement
+
+    public <T> List<T> parseLines(Class<T> tClass) {
+        Map<Field, String> names = new LinkedHashMap<>();
+        List<T> list = new ArrayList<>();
+        Field[] fields = tClass.getDeclaredFields();
+        for (Field field : fields
+        ) {
+            if (field.isAnnotationPresent(CsvHeader.class)) {
+                CsvHeader csvHeader = field.getAnnotation(CsvHeader.class);
+                if (csvHeader.required()) {
+                    names.put(field, csvHeader.name());
+                }
+            }
+            ;
+
+        }
+
+        // todo getFile
+
+
     }
 }
 
