fa4b81f0b289678851430d3307a3a57db4cbaf7f
Xitaymin <y.barashenko@gmail.com>
2021-09-18 22:12:42 +0300

    Initial commit

---
+++ src/main/java/com/xitaymin/CsvHeader.java
@@ -0,0 +1,13 @@
+package com.xitaymin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.FIELD)
+public @interface CsvHeader {
+    String name();
+    boolean required() default true;
+}
---
+++ src/main/java/com/xitaymin/CsvParser.java
@@ -0,0 +1,13 @@
+package com.xitaymin;
+
+import java.util.List;
+
+public class CsvParser {
+    public CsvParser(String filePath) {
+        // todo implement
+    }
+    public <T> List<T> parseLines(Class<T>) {
+        // todo implement
+    }
+}
+
