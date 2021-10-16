acc186e7a47cb133369a45891058fbb5401462f2
xitaymin <y.barashenko@gmail.com>
2021-10-16 19:25:23 +0300

    deleted comments

--- src/main/java/com/xitaymin/setters/StringFieldSetter.java
+++ src/main/java/com/xitaymin/setters/StringFieldSetter.java
@@ -18,16 +18,4 @@ public class StringFieldSetter<T> extends OtherFieldSetter<T> {
         return valueFromCsv.isBlank();
     }
 
-//    @Override
-//    public void setField(String valueFromCsv, T target, Field field) throws IllegalAccessException {
-//        CsvHeader csvHeader = field.getAnnotation(CsvHeader.class);
-//        if(valueFromCsv.isBlank()){
-//            if(csvHeader.required()){
-//                throw new RequiredValueAbsentException(String.format(REQUIRED_FIELD_VALUE_ABSENT, field.getName(), csvHeader.name()));
-//            }
-//            else field.set(target, DEFAULT_STRING);
-//            return;
-//        }
-//        field.set(target, valueFromCsv);
-//    }
 }
